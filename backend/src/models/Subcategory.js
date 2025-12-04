edce2274ef904a07d4dcb6d1dd684d3135d5e59f
Anne-gaëlle Bernard <anne-gaelle.bernard@laplateforme.io>
2025-12-04 15:42:40 +0100

    modification de architecture

--- backend/src/models/Subcategory.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const SubcategorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true }
-})
-
-module.exports = mongoose.model('Subcategory', SubcategorySchema)
--- backend/src/server.js
+++
@@ -1,7 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const app = require('./app');
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
